5245d28dcd740f73e43d6298edea7811b0291e8a
dsfawsd <96902481+VictorNiu1@users.noreply.github.com>
2024-08-13 19:44:57 -0700

    s

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import overlayImage from '/Users/victorniu/hopekcc/src/assets/robot.png';
+
 function Home() {
-  return (
-    <main>
-    </main>
-  );
+
 }
 
 export default Home;
